50395034cbdde8bd3acd24243ed9ca3f8e3a14ec
Jaime <jgaimer42@gmail.com>
2020-06-27 13:21:37 -0500

    hotfix

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template
 import sys
 sys.path.append("../bd/")
 sys.path.append("../src/")
-#from conexion import conexion
+from conexion import conexion
 #from controllers import caso1
 app = Flask(__name__)
 @app.route("/")
