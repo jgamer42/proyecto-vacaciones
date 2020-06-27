38e540e6a1d424e33547be581434f8c52be9094b
Jaime <jgaimer42@gmail.com>
2020-06-27 13:32:04 -0500

    hotfix

--- api/app.py
+++ api/app.py
@@ -3,8 +3,8 @@ import sys
 import os
 sys.path.append(os.getcwd()+"/bd/")
 sys.path.append(os.getcwd()+"/src/")
-from conexion import conexion
-from controllers import caso1
+#from conexion import conexion
+#from controllers import caso1
 app = Flask(__name__)
 @app.route("/")
 def index():
