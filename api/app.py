acbd6ac23df077467408bb8818a7246b70ed4835
Jaime <jgaimer42@gmail.com>
2020-06-27 13:30:03 -0500

    hotfix

--- api/app.py
+++ api/app.py
@@ -1,13 +1,11 @@
 from flask import Flask , render_template
 import sys
-sys.path.append("../bd/")
-sys.path.append("../src/")
+import os
+sys.path.append(os.getcwd()+"/bd/")
+sys.path.append(os.getcwd()+"/src/")
 from conexion import conexion
-#from controllers import caso1
+from controllers import caso1
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
