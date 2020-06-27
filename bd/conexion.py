9a4a28236fcab5fa231029ab6dab906da4b9fff8
Jaime <jgaimer42@gmail.com>
2020-06-27 14:13:42 -0500

    hotfix

--- bd/conexion.py
+++ bd/conexion.py
@@ -6,10 +6,11 @@ config.read("./bd/config.ini")
 
 class conexion():
     def __init__(self):
-        self.conexion = psycopg2.connect(host=config["db"]["host"],database=config["db"]["database"],user=config["db"]["user"],password=config["db"]["password"])
+        pass
+        '''self.conexion = psycopg2.connect(host=config["db"]["host"],database=config["db"]["database"],user=config["db"]["user"],password=config["db"]["password"])
         self.consulta = self.conexion.cursor()
         self.consulta.execute("SELECT * FROM prueba")
         self.datos = self.consulta.fetchall()
         print(self.datos)
-        self.conexion.close()
+        self.conexion.close()'''
 a = conexion()
