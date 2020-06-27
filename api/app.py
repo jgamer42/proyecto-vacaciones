0200c22854c48029c063a45944f110692802dace
Jaime <jgaimer42@gmail.com>
2020-06-27 13:33:52 -0500

    hotfix

--- api/app.py
+++ api/app.py
@@ -1,10 +1,5 @@
 from flask import Flask , render_template
-import sys
-import os
-sys.path.append(os.getcwd()+"/bd/")
-sys.path.append(os.getcwd()+"/src/")
-#from conexion import conexion
-#from controllers import caso1
+
 app = Flask(__name__)
 @app.route("/")
 def index():
