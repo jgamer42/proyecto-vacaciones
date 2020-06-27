67dc9208e532ca66d3da17ebc42e226cc5487b09
Jaime <jgaimer42@gmail.com>
2020-06-27 13:16:40 -0500

    hotfix

---
+++ bd/conexion.py
@@ -0,0 +1,17 @@
+import psycopg2
+import configparser
+config = configparser.ConfigParser()
+config.sections()
+config.read("../bd/config.ini")
+
+class conexion():
+    def __init__(self):
+        self.conexion = psycopg2.connect(host=config["db"]["host"],database=config["db"]["database"],user=config["db"]["user"],password=config["db"]["password"])
+        self.consulta = self.conexion.cursor()
+        self.consulta.execute("SELECT * FROM prueba")
+        self.datos = self.consulta.fetchall()
+        print(self.datos)
+        self.conexion.close()
+
+
+a = conexion()
