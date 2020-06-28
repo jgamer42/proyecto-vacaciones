30cd8f704f7fa6d55b751fe9998c100a7eaa78a0
Jaime <jgaimer42@gmail.com>
2020-06-28 10:13:04 -0500

    prueba deploy

--- bd/conexion.py
+++ bd/conexion.py
@@ -1,16 +1,16 @@
 import psycopg2
 import configparser
+import os 
 config = configparser.ConfigParser()
 config.sections()
-config.read("./bd/config.ini")
+config.read(os.getcwd()+"/bd/config.ini")
 
 class conexion():
     def __init__(self):
-        pass
-        '''self.conexion = psycopg2.connect(host=config["db"]["host"],database=config["db"]["database"],user=config["db"]["user"],password=config["db"]["password"])
+        self.conexion = psycopg2.connect(host=config["db"]["host"],database=config["db"]["database"],user=config["db"]["user"],password=config["db"]["password"])
         self.consulta = self.conexion.cursor()
         self.consulta.execute("SELECT * FROM prueba")
         self.datos = self.consulta.fetchall()
         print(self.datos)
-        self.conexion.close()'''
+        self.conexion.close()
 a = conexion()
