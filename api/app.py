505714638cb36628059a5ed582a68868d6078949
Jaime <jgaimer42@gmail.com>
2020-06-28 10:31:02 -0500

    peticion de prueba

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, jsonify
 import sys
 sys.path.append("./src")
 sys.path.append("./bd")
@@ -8,6 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/prueba" , methods=["GET"])
+def prueba():
+    respuesta = [{"mensaje":"este es una consulta de prueba","dato":2}]
+    return jsonify(respuesta)
 
 if __name__ == "__main__":
     app.run()
